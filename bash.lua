2e1faf12b09a419a299edeaa03cb1d95233475b5
TheLuaFox86 <peanuts005@outlook.com>
2025-04-20 17:09:48 -0400

    Update bash.lua

--- bash.lua
+++ bash.lua
@@ -1,9 +1,26 @@
 local shell = require("shell")
-local data = ...
+local mode, data = ...
+if mode == "-c" then
 for line in data:gmatch("[^\r\n]+") do
   line = line:match("^%s*(.-)%s*$") -- trim whitespace
   if line ~= "" then
     print("> " .. line)
     shell.execute(line)
   end
+end
+else
+local data = ""
+do
+  local f = io.open(mode)
+  data = f:read("*a")
+  f:flush()
+  f:close()
+end
+for line in data:gmatch("[^\r\n]+") do
+  line = line:match("^%s*(.-)%s*$") -- trim whitespace
+  if line ~= "" then
+    print("> " .. line)
+    shell.execute(line)
+  end
+end
 end
