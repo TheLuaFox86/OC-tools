cfba4ea38abb192fa82e4d5b5ba7665a984b0fd5
TheLuaFox86 <peanuts005@outlook.com>
2025-04-20 22:05:47 +0000

    Update

---
+++ tapestream.lua
@@ -0,0 +1,71 @@
+local octoolsurl = "https://raw.githubusercontent.com/TheLuaFox86/OC-tools/main/"
+local mainlistrepo = "https://raw.githubusercontent.com/TheLuaFox86/oc-music-list/main/"
+local files = {'SHADOW'}
+local bash = loadfile("/bin/bash.lua", '=bashcmd')
+bash('-c', [[
+echo "Wellcome to opentapes"
+echo "creating shortcut..."
+]])
+f = io.open('/bin/ts.lua', "w")
+f:write([[
+    loadfile('/bin/bash.lua', '=bashCmd')("-c", 'wget ' .. octoolsurl .. '/tapestream.lua && "/home/tapestream.lua" && rm /home/tapestream.lua')
+]])
+f:flush()
+f:close()
+print("just type 'ts' to start tapestream\n dont worry tapestream is online")
+local function playSong(url, name)
+    bash("-c", 'wget ' .. url .. "/" .. name .. '.dfwpm'.. "/tmp/song.dfpwm -f" .. [[
+    tape write /tmp/song.dfpwm "TapeStream: " .. name
+    echo "use tape drive to stop"
+    tape play
+    ]]
+end
+function cls()
+    bash('-c', [[
+cls
+]])
+end
+function split_quoted(str)
+    local result = {}
+    for word in str:gmatch('%b""|[^%s]+') do
+        if word:sub(1, 1) == '"' and word:sub(-1) == '"' then
+            word = word:sub(2, -2) -- remove surrounding quotes
+        end
+        table.insert(result, word)
+    end
+    return result
+end
+cls()
+local go = true
+while go do
+    local a = split_quoted(io.read("*Line"))
+    local b = {}
+    for i,v in ipairs(a) do
+        b[i]=v
+    end
+    a = b
+    if a[1] == "list" then
+        for i, v in ipairs(files) do
+            print(v)
+        end
+    elseif a[1] == 'play' then
+        local b = a[2]
+        if tonumber(a[2]) then
+            for i,v in ipairs(files) do
+                if tonumber(a[2]) == i then
+                    b = v
+                end
+            end
+        end
+        playSong(mainlistrepo, b)
+    elseif a[1] == "exit" then
+        go = false
+    else
+        print([[
+            --Commands--
+            list: Lists all available songd
+            play: play a song usage: play <song: number or string>
+            exit: exit out of TapeStream
+        ]])
+    end
+end
