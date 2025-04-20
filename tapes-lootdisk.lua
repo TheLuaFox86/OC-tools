26675e2d0f3a9c0bfa47f7a27a917729d2bc1519
TheLuaFox86 <peanuts005@outlook.com>
2025-04-19 22:43:06 -0400

    Update tapes-lootdisk.lua

--- tapes-lootdisk.lua
+++ tapes-lootdisk.lua
@@ -1,9 +1,9 @@
 print("bash must be installed")
 loadfile("/bin/bash.lua", "t")([[
-wget https://raw.githubusercontent.com/TheLuFox86/OC-tools/main/init.lua tapeloader-bios.lua
+wget https://raw.githubusercontent.com/TheLuaFox86/OC-tools/main/init.lua tapeloader-bios.lua
 mkdir bin
-wget https://raw.githubusercontent.com/TheLuFox86/OC-tools/main/write.lua bin/tbWrite.lua
-wget https://raw.githubusercontent.com/TheLuFox86/OC-tools/main/read.lua bin/tbRead.lua
-wget https://raw.githubusercontent.com/TheLuFox86/OC-tools/main/dumpbios.lua bin/dumpbios.lua
-wget https://raw.githubusercontent.com/TheLuFox86/OC-tools/main/lootdisk-prop.lua .prop
+wget https://raw.githubusercontent.com/TheLuaFox86/OC-tools/main/write.lua bin/tbWrite.lua
+wget https://raw.githubusercontent.com/TheLuaFox86/OC-tools/main/read.lua bin/tbRead.lua
+wget https://raw.githubusercontent.com/TheLuaFox86/OC-tools/main/dumpbios.lua bin/dumpbios.lua
+wget https://raw.githubusercontent.com/TheLuaFox86/OC-tools/main/lootdisk-prop.lua .prop
 ]])
