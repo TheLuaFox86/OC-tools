6a484ac785ccacc5874a39aa3a040fa661bf50d7
TheLuaFox86 <peanuts005@outlook.com>
2025-04-21 00:47:06 +0000

    ok

--- oct-updater.lua
+++ oct-updater.lua
@@ -1,7 +1,8 @@
 local bash = loadfile("/bin/bash.lua")
 print("Updating...")
 bash("-c", "wget https://raw.githubusercontent.com/TheLuaFox86/OC-tools/refs/heads/main/update-config.lua /lib/updc.lua -f")
-local url, tb = require("updc")
+local b = require("updc")
+local url, tb = b.url, b.tb
 os.setenv("UURL", url)
 for _, v in ipairs(tb) do
     bash("-c", v)
