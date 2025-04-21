98e733d1461509c2e6e76d7d55b366393249173f
TheLuaFox86 <peanuts005@outlook.com>
2025-04-21 00:10:11 +0000

    update bash

--- bash.lua
+++ bash.lua
@@ -4,7 +4,6 @@ if mode == "-c" then
 for line in data:gmatch("[^\r\n]+") do
   line = line:match("^%s*(.-)%s*$") -- trim whitespace
   if line ~= "" then
-    print("> " .. line)
     shell.execute(line)
   end
 end
