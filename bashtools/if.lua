c4ff907138c5dd198548462bdca74ec5b0dbffd5
TheLuaFox86 <peanuts005@outlook.com>
2025-04-21 17:06:25 +0000

    fix

--- bashtools/if.lua
+++ bashtools/if.lua
@@ -1,5 +1,5 @@
 local a = table.pack(...)
 local bool = load("a = table.pack(...)\n return a[1] " .. a[2] .. " a[2]", "=bool", "t")(a[1], a[3])
 if bool then
-    require("shell").execute("bash", a[4], a[5])
+    loadfile("/bin/bash.lua", "t")(a[4], a[5])
 end
